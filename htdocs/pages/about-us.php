592494309f26b3e6590202682b2250d95e053c5c
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-05 15:38:26 +0330

    fix tabs

--- htdocs/pages/about-us.php
+++ htdocs/pages/about-us.php
@@ -1,35 +1,34 @@
-	<div class="mh10 mla font gallery sample">
-		<header class="mh3 mt3 ml1 head">
-			<h2>typography</h2>
-		</header>
-		<div class="pl1 pr1 pb4 mt5 content">
-			<h1>1st example : about rug 1</h1>
-			<p>
-				Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
-			</p>
-			<h1>2nd example : about rug 2</h1>
-			<p>
-				Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
-			</p>
-			<h1>3rd example : about rug 3</h1>
-			<p>
-				Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
-			</p>
-			<h1>4th example : about rug 4</h1>
-			<p>
-				Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
-			</p>
-			<h1>5th example : about rug 5</h1>
-			<p>
-				Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
-			</p>
-			<h1>6th example : about rug 6</h1>
-			<p>
-				Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
-			</p>
-		</div>
-		<footer class="pr1 pl1 mh8 cover footers">
-			<?php include 'inc/copy-right.php'; ?>
-		</footer>
+<div class="mh10 mla font gallery sample">
+	<header class="mh3 mt3 ml1 head">
+		<h2>typography</h2>
+	</header>
+	<div class="pl1 pr1 pb4 mt5 content">
+		<h1>1st example : about rug 1</h1>
+		<p>
+			Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
+		</p>
+		<h1>2nd example : about rug 2</h1>
+		<p>
+			Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
+		</p>
+		<h1>3rd example : about rug 3</h1>
+		<p>
+			Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
+		</p>
+		<h1>4th example : about rug 4</h1>
+		<p>
+			Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
+		</p>
+		<h1>5th example : about rug 5</h1>
+		<p>
+			Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
+		</p>
+		<h1>6th example : about rug 6</h1>
+		<p>
+			Dolor, porttitor placerat nascetur et est enim elementum, turpis integer? Sit dapibus nunc! A pellentesque porttitor tortor tristique montes, phasellus integer tortor? Et mus ac augue, scelerisque sit a nec et purus phasellus aliquam dignissim, magna nec? Turpis a? Amet turpis turpis augue ultrices lundium integer! Nec ultrices non auctor magna nunc duis sed amet, turpis lacus parturient lacus adipiscing turpis elementum, nunc proin sed placerat proin tempor.
+		</p>
 	</div>
+	<footer class="pr1 pl1 mh8 cover footers">
+		<?php include 'inc/copy-right.php'; ?>
+	</footer>
 </div>
--- htdocs/pages/contact-us.php
+++ htdocs/pages/contact-us.php
@@ -1,37 +1,36 @@
-		<div class="mh10 mla font contact-us">
-			<header class="mh3 mt3 ml1 pb5 head">
-				<h2>contact us</h2>
-			</header>
-			<div class="pl1 pr1 pb5 content cover">
-				<section class="w9 mh10 pr1 left form">
-					<form action="">
-						<abbr title="Name">
-							<input type="text" name="Name" placeholder='Name' />
-						</abbr>
-						<abbr title="Email">
-							<input type="email" name="usremail" placeholder='E-mail' />
-						</abbr>
-						<abbr title="Message">
-							<textarea name="Message" id="Message" cols="30" rows="10" placeholder='Message'></textarea>
-						</abbr>
-						<abbr title="Submit">
-							<input class="font btn_submit " id='btn_submit' type="submit" value="Send Message" />
-						</abbr>
-					</form>
-				</section>
-				<section class="w7 mh10 mla info">
-					<h3 class='pb2 font'>get in touch with us!</h3>
-					<ul class='pb2'>
-						<li style="background:url('./images/icons/phone_Android.png') no-repeat left 9px ">98-555-666-7777</li>
-						<li style="background:url('./images/icons/fax.png') no-repeat left 9px ">98-555-666-7777</li>
-						<li style="background:url('./images/icons/email.png') no-repeat left 9px ">contact@swastikarugs.pro</li>
-						<li style="background:url('./images/icons/house.png') no-repeat left 9px ">No 7, Tir St, Hafez Blv, Mashad, Iran</li>
-					</ul>
-					<iframe width="210" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=36.324202,59.491592&amp;num=1&amp;t=h&amp;vpsrc=0&amp;ie=UTF8&amp;z=14&amp;iwloc=near&amp;ll=36.324303,59.491466&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?q=36.324202,59.491592&amp;num=1&amp;t=h&amp;vpsrc=0&amp;ie=UTF8&amp;z=14&amp;iwloc=near&amp;ll=36.324303,59.491466&amp;source=embed" style="color:#0000FF;text-align:left"></a></small>
-				</section>
-			</div>
-<footer class="pr1 pl1 mh8 cover footers">
-			<?php include 'inc/copy-right.php'; ?>
-		</footer>
+<div class="mh10 mla font contact-us">
+	<header class="mh3 mt3 ml1 pb5 head">
+		<h2>contact us</h2>
+	</header>
+	<div class="pl1 pr1 pb5 content cover">
+		<section class="w9 mh10 pr1 left form">
+			<form action="">
+				<abbr title="Name">
+					<input type="text" name="Name" placeholder='Name' />
+				</abbr>
+				<abbr title="Email">
+					<input type="email" name="usremail" placeholder='E-mail' />
+				</abbr>
+				<abbr title="Message">
+					<textarea name="Message" id="Message" cols="30" rows="10" placeholder='Message'></textarea>
+				</abbr>
+				<abbr title="Submit">
+					<input class="font btn_submit " id='btn_submit' type="submit" value="Send Message" />
+				</abbr>
+			</form>
+		</section>
+		<section class="w7 mh10 mla info">
+			<h3 class='pb2 font'>get in touch with us!</h3>
+			<ul class='pb2'>
+				<li style="background:url('./images/icons/phone_Android.png') no-repeat left 9px ">98-555-666-7777</li>
+				<li style="background:url('./images/icons/fax.png') no-repeat left 9px ">98-555-666-7777</li>
+				<li style="background:url('./images/icons/email.png') no-repeat left 9px ">contact@swastikarugs.pro</li>
+				<li style="background:url('./images/icons/house.png') no-repeat left 9px ">No 7, Tir St, Hafez Blv, Mashad, Iran</li>
+			</ul>
+			<iframe width="210" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=36.324202,59.491592&amp;num=1&amp;t=h&amp;vpsrc=0&amp;ie=UTF8&amp;z=14&amp;iwloc=near&amp;ll=36.324303,59.491466&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?q=36.324202,59.491592&amp;num=1&amp;t=h&amp;vpsrc=0&amp;ie=UTF8&amp;z=14&amp;iwloc=near&amp;ll=36.324303,59.491466&amp;source=embed" style="color:#0000FF;text-align:left"></a></small>
+		</section>
 	</div>
+	<footer class="pr1 pl1 mh8 cover footers">
+		<?php include 'inc/copy-right.php'; ?>
+	</footer>
 </div>
