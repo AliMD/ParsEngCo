0c96ceff0e7ca88dfa111de70f434073d5ea80ca
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-05 15:44:04 +0330

    products and projects: remove junks and fix tabs

--- htdocs/pages/products.php
+++ htdocs/pages/products.php
@@ -1,180 +1,117 @@
-		<div class="mh10 mla font gallery">
-			<header class="mh3 mt3 ml1 head">
-				<h2>galleries: 3 columns</h2>
-			</header>
-			<div class="pl1 pr1 pb4 content">
-				<section class="mh5 mt5 row cover">
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug1.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug1.jpg" alt="rug1" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug2.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug2.jpg" alt="rug2" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box mla">
-						<a href="./images/galleries/original/rug3.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug3.jpg" alt="rug3" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-				</section>
-				<section class="mh5 mt1 row cover">
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug4.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug4.jpg" alt="rug4" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug5.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug5.jpg" alt="rug5" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box mla">
-						<a href="./images/galleries/original/rug6.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug6.jpg" alt="rug6" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-				</section>
-				<section class="mh5 mt1 row cover">
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug7.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug7.jpg" alt="rug7" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug8.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug8.jpg" alt="rug8" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box mla">
-						<a href="./images/galleries/original/rug9.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug9.jpg" alt="rug9" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-				</section>
-				<section class="mh5 mt1 row cover">
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug10.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug10.jpg" alt="rug10" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug11.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug11.jpg" alt="rug11" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box mla">
-						<a href="./images/galleries/original/rug12.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug12.jpg" alt="rug12" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-				</section>
-				<section class="mh5 mt1 row cover">
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug13.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug13.jpg" alt="rug13" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box left">
-						<a href="./images/galleries/original/rug14.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug14.jpg" alt="rug14" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-					<div class="w7 mh9 box mla">
-						<a href="./images/galleries/original/rug15.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
-							<img src="./images/galleries/thumbs/rug15.jpg" alt="rug15" width="196" height="145" />
-							<span></span>
-						</a>
-					</div>
-				</section>
-				<section class="mt3 text">
-					<p>
-						This is just an example of some text. 
-						Text on gallery pages can be inserted either Above or Below depending on your selection. 
-						Galleries can also be created either as a Page or Shortcode with a variety of additional options available to customize as desired.	
-					</p>
-				</section>
+<div class="mh10 mla font gallery">
+	<header class="mh3 mt3 ml1 head">
+		<h2>galleries: 3 columns</h2>
+	</header>
+	<div class="pl1 pr1 pb4 content">
+		<section class="mh5 mt5 row cover">
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug1.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug1.jpg" alt="rug1" width="196" height="145" />
+					<span></span>
+				</a>
 			</div>
-			<footer class="pr1 pl1 mh8 cover footers">
-				<section class="mt1 pb5 fmenu ">
-					<section class="w5 mh10 mt1 left learn_more">
-						<h3>learn more</h3>
-						<ul>
-							<li><a href="#">blog</a></li>
-							<li><a href="#">contact us</a></li>
-							<li><a href="#">portfolio</a></li>
-						</ul>
-					</section>
-					<section class="w5 mh10 mt1 left get_in_touch">
-						<h3>get in touch</h3>
-						<ul>
-							<li><a href="#">contat us today</a></li>
-							<li><a href="#">denoizzed</a></li>
-							<li><a href="#">our web media</a></li>
-						</ul>
-					</section>
-					<section class="w5 mh9 mt1 mla need_to_know">
-						<h3>need to know</h3>
-						<p>
-							© 2012, Swastika Rugs.<br />
-							Include tagline if you want to.<br />
-							This footer is Ready x 3.
-						</p>			
-					</section>
-				</section>
-				<section class="mh3 mt1 pb2 cover">
-					<div class="w5 left copyright">
-						<span>© 2012 Swastika Rugs Team</span>
-					</div>
-					<div class="w10 mla social">
-						<ul class="pl1">
-							<li><abbr title="Facebook"><a class='fb' href="http://www.facebook.com" target="_blank"></a></abbr></li>
-							<li><abbr title="Twitter"><a class='tw' href="https://twitter.com" target="_blank"></a></abbr></li>
-							<li><abbr title="LinkedIn"><a class='in' href="http://www.linkedin.com" target="_blank"></a></abbr></li>
-							<li><abbr title="Picasa"><a class='picasa' href="http://picasa.google.com" target="_blank"></a></abbr></li>
-							<li><abbr title="Github"><a class='github' href="https://github.com" target="_blank"></a></abbr></li>
-							<li><abbr title="Skype"><a class='skype' href="http://www.skype.com" target="_blank"></a></abbr></li>
-							<li><abbr title="YouTube"><a class='yt' href="http://www.youtube.com" target="_blank"></a></abbr></li>
-							<li><abbr title="RSS"><a class='rss' href="#" target="_blank"></a></abbr></li>
-						</ul>
-					</div>
-				</section>
-			</footer>
-		</div>
-	</div>
-	<section class="slideshow">
-		<div class="noise"></div>
-		<section class="background">
-			<div style="background-image:url('./images/backgrounds/bg1.jpg');"></div>
-			<div style="background-image:url('./images/backgrounds/bg2.jpg');"></div>
-			<div style="background-image:url('./images/backgrounds/bg3.jpg');"></div>
-			<div style="background-image:url('./images/backgrounds/bg4.jpg');"></div>
-			<div style="background-image:url('./images/backgrounds/bg5.jpg');"></div>
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug2.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug2.jpg" alt="rug2" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box mla">
+				<a href="./images/galleries/original/rug3.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug3.jpg" alt="rug3" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
 		</section>
-	</section>
-
-	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
-	<script type="text/javascript" src="./scripts/script.js"></script>
-	<script type="text/javascript" src="scripts/underdev.js"></script>
-	<script type="text/javascript" src="./scripts/jquery.js"></script>
-	<script type="text/javascript" src="./scripts/jquery-ui.js"></script>
-	<script type="text/javascript" src="./scripts/lightbox.js"></script>
-	<script src="https://apis.google.com/js/plusone.js"></script>
-</body>
-</html>
+		<section class="mh5 mt1 row cover">
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug4.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug4.jpg" alt="rug4" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug5.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug5.jpg" alt="rug5" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box mla">
+				<a href="./images/galleries/original/rug6.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug6.jpg" alt="rug6" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+		</section>
+		<section class="mh5 mt1 row cover">
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug7.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug7.jpg" alt="rug7" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug8.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug8.jpg" alt="rug8" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box mla">
+				<a href="./images/galleries/original/rug9.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug9.jpg" alt="rug9" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+		</section>
+		<section class="mh5 mt1 row cover">
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug10.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug10.jpg" alt="rug10" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug11.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug11.jpg" alt="rug11" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box mla">
+				<a href="./images/galleries/original/rug12.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug12.jpg" alt="rug12" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+		</section>
+		<section class="mh5 mt1 row cover">
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug13.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug13.jpg" alt="rug13" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box left">
+				<a href="./images/galleries/original/rug14.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug14.jpg" alt="rug14" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+			<div class="w7 mh9 box mla">
+				<a href="./images/galleries/original/rug15.jpg" title="Item 1 title ..." data-desc="Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/thumbs/rug15.jpg" alt="rug15" width="196" height="145" />
+					<span></span>
+				</a>
+			</div>
+		</section>
+		<section class="mt3 text">
+			<p>
+				This is just an example of some text. 
+				Text on gallery pages can be inserted either Above or Below depending on your selection. 
+				Galleries can also be created either as a Page or Shortcode with a variety of additional options available to customize as desired.	
+			</p>
+		</section>
+	</div>
+	<footer class="pr1 pl1 mh8 cover footers">
+	<?php include 'inc/copy-right.php'; ?>
+	</footer>
+</div>
