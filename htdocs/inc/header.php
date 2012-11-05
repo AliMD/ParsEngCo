a5ca3301731c6709f0250a2548e7acd48d4f3e33
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-05 14:54:49 +0330

    dynamic title

--- htdocs/inc/header.php
+++ htdocs/inc/header.php
@@ -2,7 +2,7 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8" />
-	<title>.:: SWASTIKA RUGS ::.</title>
+	<title>Pars Engineering Co. | <?php echo $title ?></title>
 	<link rel="stylesheet" type="text/css" href="1styles.css" />
 	<link rel="stylesheet" type="text/css" href="style.css" />
 	<link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700" rel="stylesheet" type="text/css" />
