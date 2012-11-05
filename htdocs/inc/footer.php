02a23397a798c35e8ff3b686917054f71cbe946f
Ali Mihandoost <i@ali.md>
2012-11-05 16:35:39 +0330

    fix bug - remove under link its merged to script.js

--- htdocs/inc/footer.php
+++ htdocs/inc/footer.php
@@ -1,6 +1,5 @@
-</div>
+	</div>
 	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
-	<script type="text/javascript" src="scripts/underdev.js"></script>
 	<script type="text/javascript" src="./scripts/script.js"></script>
 </body>
 </html>
