72e6a3fbeafabdfff3b73fdf45c10d0496ff60ec
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-05 15:50:42 +0330

    change menu

--- htdocs/inc/menu.php
+++ htdocs/inc/menu.php
@@ -3,7 +3,7 @@
 		<div class="menu">
 			<div class="logo cover">
 				<h1>
-					<a href="index.html" >
+					<a href="home" >
 						<span>swastika</span>
 					</a>
 				</h1>	
@@ -11,129 +11,31 @@
 			<nav >
 				<menu>
 					<li>
-						<a href="index.html" style="color: #fff;">
+						<a href="home" style="color: #fff;">
 							home
 							<span>back to home page</span>
 						</a>
 					</li>
 					<li>
-						<a href="#">
-							examples
-							<span>what we're offering</span>
+						<a href="about-us">
+							about us
+							<span>what we do</span>
 						</a>
-						<menu>
-							<li>
-								<a href="#">submenu 1st</a>
-								<menu>
-									<li>
-										<a href="#">submenu 2nd</a>
-										<menu>
-											<li>
-												<a href="#">submenu 3rd</a>
-											</li>
-										</menu>
-									</li>
-								</menu>
-							</li>
-							<li>
-								<a href="typo.html">typography</a>
-							</li>
-							<li>
-								<a href="sample.html">example page</a>
-							</li>
-							<li>
-								<a href="#">more example</a>
-								<menu>
-									<li>
-										<a href="sample.html">sample page</a>
-									</li>
-									<li>
-										<a href="pass-pro.html">password protect</a>
-									</li>
-								</menu>
-							</li>
-						</menu>
 					</li>
 					<li>
-						<a href="#">
-							galleries
-							<span>what we're proud of</span>
-						</a>
-						<menu>
-							<li>
-								<a href="#">color box</a>
-								<menu>
-									<li>
-										<a href="gallerycol2.html">2 columns</a>
-									</li>
-									<li>
-										<a href="gallerycol3.html">3 columns</a>
-									</li>
-								</menu>
-							</li>
-							<li>
-								<a href="#">fancy box</a>
-								<menu>
-									<li>
-										<a href="gallerycol2.html">2 columns</a>
-									</li>
-									<li>
-										<a href="gallerycol3.html">3 columns</a>
-									</li>
-								</menu>
-							</li>
-							<li>
-								<a href="galleria.html">galleria</a>
-							</li>
-							<li>
-								<a href="#">pretty photo</a>
-								<menu>
-									<li>
-										<a href="gallerycol2.html">picture 1</a>
-									</li>
-									<li>
-										<a href="gallerycol3.html">picture 2</a>
-									</li>
-								</menu>
-							</li>
-							<li>
-								<a href="sliderview.html">slider view</a>
-							</li>
-						</menu>
-					</li>
-					<li>
-						<a href="#">
-							portfolios
+						<a href="products">
+							products
 							<span>show casing our work</span>
 						</a>
-						<menu>
-							<li>
-								<a href="#">designing</a>
-								<menu>
-									<li>
-										<a href="port-mode.html">2 columns</a>
-									</li>
-									<li>
-										<a href="port-mode2.html">3 columns</a>
-									</li>
-								</menu>
-							</li>
-							<li>
-								<a href="port-mode.html">photos</a>
-							</li>
-							<li>
-								<a href="port-mode2.html">models</a>
-							</li>
-						</menu>
 					</li>
 					<li>
-						<a href="blog.html">
-							blog
-							<span>latest news & tutorials</span>
+						<a href="projects">
+							projects
+							<span>latest projects</span>
 						</a>
 					</li>
 					<li>
-						<a href="contact-us.html">
+						<a href="contact-us">
 							contact us
 							<span>get in touch</span>
 						</a>
