38830eb94b0237f10d99e644e91f1d774e3536d6
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-05 12:34:54 +0330

    add header inc

--- htdocs/Pages/home.php
+++ htdocs/Pages/home.php
@@ -1,15 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8" />
-	<title>.:: SWASTIKA RUGS ::.</title>
-	<link rel="stylesheet" type="text/css" href="1styles.css" />
-	<link rel="stylesheet" type="text/css" href="style.css" />
-	<link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700" rel="stylesheet" type="text/css" />
-	<script type="text/javascript" src="./scripts/html5shiv.js"></script>
-</head>
-<body>
-	<div class="w24 mlra container mh40">
+<?php include './inc/header.php'; ?>
 		<div class="open" id="anim">
 			<div id="menu" class="left menu_wrap">
 				<div class="menu">
