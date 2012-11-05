54cd969df7a3af6501d39e12db7b71fbcc119a4a
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-05 14:43:21 +0330

    expoload everything :D

---
+++ htdocs/inc/footer.php
@@ -0,0 +1,6 @@
+</div>
+	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
+	<script type="text/javascript" src="scripts/underdev.js"></script>
+	<script type="text/javascript" src="./scripts/script.js"></script>
+</body>
+</html>
--- htdocs/pages/home.php
+++ htdocs/pages/home.php
@@ -1,19 +1,16 @@
 <?php include './inc/header.php'; ?>
 <?php include './inc/menu.php'; ?>
 
-	<section class="slideshow">
-		<div class="noise"></div>
-		<section class="background">
-			<div style="background-image:url('./images/backgrounds/bg1.jpg');"></div>
-			<div style="background-image:url('./images/backgrounds/bg2.jpg');"></div>
-			<div style="background-image:url('./images/backgrounds/bg3.jpg');"></div>
-			<div style="background-image:url('./images/backgrounds/bg4.jpg');"></div>
-			<div style="background-image:url('./images/backgrounds/bg5.jpg');"></div>
-		</section>
+</div>
+<section class="slideshow">
+	<div class="noise"></div>
+	<section class="background">
+		<div style="background-image:url('./images/backgrounds/bg1.jpg');"></div>
+		<div style="background-image:url('./images/backgrounds/bg2.jpg');"></div>
+		<div style="background-image:url('./images/backgrounds/bg3.jpg');"></div>
+		<div style="background-image:url('./images/backgrounds/bg4.jpg');"></div>
+		<div style="background-image:url('./images/backgrounds/bg5.jpg');"></div>
 	</section>
+</section>
 
-	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
-	<script type="text/javascript" src="scripts/underdev.js"></script>
-	<script type="text/javascript" src="./scripts/script.js"></script>
-</body>
-</html>
+<?php include './inc/footer.php'; ?>
