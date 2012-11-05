52492af5ce3139bc298e4df0c202f4f6c98dd8e5
Ali Mihandoost <i@ali.md>
2012-11-05 11:57:32 +0330

    simple template engine

---
+++ htdocs/theme.php
@@ -0,0 +1,22 @@
+<?php
+
+function inc($filename,$folder='inc'){
+	@include "$folder/$filename.php";
+}
+
+
+$content = isset($_GET[page]) ? $_GET[page] : 'home';
+$title = ucwords(str_replace(array('-','%20'), ' ', $content));
+
+@file_exists("./page/$content.php") or $content='404';
+
+
+// view template
+
+inc('header');
+
+inc('menu');
+
+inc($content,'pages');
+
+inc('footer');
