e50cecb069d88663245933ebd1ad8bb6b770aa71
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-06 16:26:55 +0330

    persian projects page

--- htdocs/pages/projects.php
+++ htdocs/pages/projects.php
@@ -5,106 +5,99 @@
 	<div class="pl1 pr1 pb4 content">
 		<section class="mh5 mt5 row cover">
 			<article class="w7 mh9 box left">
-				<h3> photo portfolio #1</h3>
+				<h3> پروژه 1</h3>
 				<a href="./images/galleries/original/rug1.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
 					<img src="./images/galleries/thumbs2/rug1.jpg" alt="rug1" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel leo ut orci pellentesque facilisis.
+					 تنوع طرح و رنگ بسیار بالای این محصول می تواند محدودیت را از دید و ذهن طراحان دکوراسیون داخلی و ایده پردازان صنعت تزئینات برداشته
 					<br />
 				</p>
 			</article>
 			<article class="w7 mh9 box mla">
-				<h3> photo portfolio #2</h3>
+				<h3> پروزه 2</h3>
 				<a href="./images/galleries/original/rug2.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
 					<img src="./images/galleries/thumbs2/rug2.jpg" alt="rug2" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel leo ut orci pellentesque facilisis.
+					 تنوع طرح و رنگ بسیار بالای این محصول می تواند محدودیت را از دید و ذهن طراحان دکوراسیون داخلی و ایده پردازان صنعت تزئینات برداشته
 					<br />
 				</p>
 			</article>
 		</section>
 		<section class="mh5 mt5 row cover">
 			<article class="w7 mh9 box left">
-				<h3> photo portfolio #3</h3>
+				<h3> پروژه 3</h3>
 				<a href="./images/galleries/original/rug3.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
 					<img src="./images/galleries/thumbs2/rug3.jpg" alt="rug3" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel leo ut orci pellentesque facilisis.
+					 تنوع طرح و رنگ بسیار بالای این محصول می تواند محدودیت را از دید و ذهن طراحان دکوراسیون داخلی و ایده پردازان صنعت تزئینات برداشته
 					<br />
 				</p>
 			</article>
 			<article class="w7 mh9 box mla">
-				<h3> photo portfolio #4</h3>
+				<h3> پروژه 4</h3>
 				<a href="./images/galleries/original/rug4.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
 					<img src="./images/galleries/thumbs2/rug4.jpg" alt="rug4" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel leo ut orci pellentesque facilisis.
+					 تنوع طرح و رنگ بسیار بالای این محصول می تواند محدودیت را از دید و ذهن طراحان دکوراسیون داخلی و ایده پردازان صنعت تزئینات برداشته
 					<br />
 				</p>
 			</article>
 		</section>
 		<section class="mh5 mt5 row cover">
 			<article class="w7 mh9 box left">
-				<h3> photo portfolio #5</h3>
+				<h3> پروژه 5</h3>
 				<a href="./images/galleries/original/rug5.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
 					<img src="./images/galleries/thumbs2/rug5.jpg" alt="rug5" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel leo ut orci pellentesque facilisis.
+					 تنوع طرح و رنگ بسیار بالای این محصول می تواند محدودیت را از دید و ذهن طراحان دکوراسیون داخلی و ایده پردازان صنعت تزئینات برداشته
 					<br />
 				</p>
 			</article>
 			<article class="w7 mh9 box mla">
-				<h3> photo portfolio #6</h3>
+				<h3>پروژه 6</h3>
 				<a href="./images/galleries/original/rug6.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
 					<img src="./images/galleries/thumbs2/rug6.jpg" alt="rug6" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel leo ut orci pellentesque facilisis.
+					 تنوع طرح و رنگ بسیار بالای این محصول می تواند محدودیت را از دید و ذهن طراحان دکوراسیون داخلی و ایده پردازان صنعت تزئینات برداشته
 					<br />
 				</p>
 			</article>
 		</section>
 		<section class="mh5 mt5 row cover">
 			<article class="w7 mh9 box left">
-				<h3> photo portfolio #7</h3>
+				<h3>پروژه 7</h3>
 				<a href="./images/galleries/original/rug7.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
 					<img src="./images/galleries/thumbs2/rug7.jpg" alt="rug7" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel leo ut orci pellentesque facilisis.
+					 تنوع طرح و رنگ بسیار بالای این محصول می تواند محدودیت را از دید و ذهن طراحان دکوراسیون داخلی و ایده پردازان صنعت تزئینات برداشته
 					<br />
 				</p>
 			</article>
 			<article class="w7 mh9 box mla">
-				<h3> photo portfolio #8</h3>
+				<h3>پروژه 8</h3>
 				<a href="./images/galleries/original/rug8.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
 					<img src="./images/galleries/thumbs2/rug8.jpg" alt="rug8" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel leo ut orci pellentesque facilisis.
+					 تنوع طرح و رنگ بسیار بالای این محصول می تواند محدودیت را از دید و ذهن طراحان دکوراسیون داخلی و ایده پردازان صنعت تزئینات برداشته
 					<br />
 				</p>
 			</article>
-		</section>
-		<section class="mt3 text">
-			<p>
-				This is just an example of some text. 
-				Text on gallery pages can be inserted either Above or Below depending on your selection. 
-				Galleries can also be created either as a Page or Shortcode with a variety of additional options available to customize as desired.	
-			</p>
 		</section>
 	</div>
 	<footer class="pr1 pl1 mh8 cover footers">
