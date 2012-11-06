7e073e51db553c1b688994b1452e94eb1f202642
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-06 13:34:35 +0330

    add contactForm JS

--- htdocs/scripts/script.js
+++ htdocs/scripts/script.js
@@ -62,4 +62,52 @@
 		return false;
 	});
 
+	//Contact Form
+function validateText(str,len){
+	return str.length >= len;
+}
+
+function validateEmail(str){
+	var emailPattern = /^[a-z0-9+_%.-]+@(?:[a-z0-9-]+\.)+[a-z]{2,6}$/i ;
+
+	return emailPattern.test(str);
+}
+
+$('#contact-form').submit(function(){
+	var target, err = false;
+
+	target = $('#name');
+	if( validateText(target.val(),3) ){
+		target.removeClass('err').addClass('ok');
+	}else{
+		target.removeClass('ok').addClass('err');
+		err = true;
+	}
+
+	target = $('#mail');
+	if( validateEmail(target.val()) ){
+		target.removeClass('err').addClass('ok');
+	}else{
+		target.removeClass('ok').addClass('err');
+		err = true;
+	}
+
+	target = $('#txt');
+	if( validateText(target.val(),10) ){
+		target.removeClass('err').addClass('ok');
+	}else{
+		target.removeClass('ok').addClass('err');
+		err = true;
+	}
+
+	if(!err){
+		$('#ifrm').animate({
+			height:'75px'
+		},500);
+	}
+
+	return !err;
+});
+
+
 })(window.Zepto || window.jQuery);
