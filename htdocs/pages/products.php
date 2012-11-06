94221b9164aac168847c4b8b675c8247fd038edd
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-06 15:07:52 +0330

    make it work with php

--- htdocs/pages/products.php
+++ htdocs/pages/products.php
@@ -3,6 +3,28 @@
 		<h2>محصولات</h2>
 	</header>
 	<div class="pl1 pr1 pb4 content">
+		<?php
+
+			$dir = "./images/galleries/products";
+			$allowed_type = array('jpg','jpeg','png','gif');
+
+			$d = dir($dir);
+			while( $f = $d->read() ){
+				$file_type = end(explode('.',$f));
+				if( !in_array(strtolower($file_type),$allowed_type) ) continue;
+
+				$file_name = substr($f,0,-strlen($file_type)-1);
+
+				echo
+					"<div class='box left'>
+					<a href='$dir/$file_name.$file_type'>
+					<img src='$dir/thumbs/$file_name.$file_type' alt='$file_name' width='196' height='145' />
+					<span></span>
+					</a>
+					</div>";				
+			}
+			
+		?>
 	<footer class="pr1 pl1 mh8 cover footers">
 	<?php include 'inc/copy-right.php'; ?>
 	</footer>
