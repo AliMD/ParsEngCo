8d31739adaaa4af0c5b43aeb36fbe7b8e69cffef
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-06 11:17:27 +0330

    fix contact-us fonts , positions and styles :D

--- htdocs/pages/contact-us.php
+++ htdocs/pages/contact-us.php
@@ -3,7 +3,7 @@
 		<h2>تماس با ما</h2>
 	</header>
 	<div class="pl1 pr1 pb5 content cover">
-		<section class="w9 mh10 pr1 left form">
+		<section class="w9 right form">
 			<form action="">
 				<abbr title="نام">
 					<input type="text" name="Name" placeholder='نام' />
@@ -19,13 +19,13 @@
 				</abbr>
 			</form>
 		</section>
-		<section class="w7 mh10 mla info">
+		<section class="w7 mh10 mr10 info">
 			<h3 class='pb2 font'>راههای تماس با ما!</h3>
-			<ul class='pb2'>
-				<li style="background:url('./images/icons/phone_Android.png') no-repeat right 9px ">98-555-666-7777</li>
-				<li style="background:url('./images/icons/fax.png') no-repeat right 9px ">98-555-666-7777</li>
-				<li style="background:url('./images/icons/email.png') no-repeat right 9px ">contact@swastikarugs.pro</li>
-				<li style="background:url('./images/icons/house.png') no-repeat right 9px ">No 7, Tir St, Hafez Blv, Mashad, Iran</li>
+			<ul class='pb2' lang="en-US">
+				<li style="background:url('./images/icons/phone_Android.png') no-repeat right 9px ">+98 915 41 41 40 2</li>
+				<li style="background:url('./images/icons/phone_Android.png') no-repeat right 9px ">+98 915 324 1559</li>
+				<li style="background:url('./images/icons/email.png') no-repeat right 9px ">info@parseng.co</li>
+				<li lang="fa-IR" class="fa" style="background:url('./images/icons/house.png') no-repeat right 9px ">مشهد ، کوچه اونجا ، پلاک همونجا</li>
 			</ul>
 			<iframe width="210" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=36.324202,59.491592&amp;num=1&amp;t=h&amp;vpsrc=0&amp;ie=UTF8&amp;z=14&amp;iwloc=near&amp;ll=36.324303,59.491466&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?q=36.324202,59.491592&amp;num=1&amp;t=h&amp;vpsrc=0&amp;ie=UTF8&amp;z=14&amp;iwloc=near&amp;ll=36.324303,59.491466&amp;source=embed" style="color:#0000FF;text-align:left"></a></small>
 		</section>
