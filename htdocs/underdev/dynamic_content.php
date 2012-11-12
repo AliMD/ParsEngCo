12aa408ccd3936966666936782d136e5ab12ff4e
Ali Mihandoost <i@ali.md>
2012-11-12 17:50:42 +0330

    fix show warning err

--- htdocs/underdev/dynamic_content.php
+++ htdocs/underdev/dynamic_content.php
@@ -2,7 +2,7 @@
 
 function get_dynamic_content($file_name,$befor='',$after=''){
 	$file_path = "./contents/{$file_name}.txt";
-	$content = file_get_contents($file_path);
+	$content = @file_get_contents($file_path);
 	if(strlen($content)>0){
 		return $befor.$content.$after;
 	}
