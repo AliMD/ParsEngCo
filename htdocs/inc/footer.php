fad31e4f6c974fcf73844a49355611c6311e157d
Ali Mihandoost <i@ali.md>
2012-11-15 12:07:41 +0330

    add colorbox core

--- htdocs/inc/footer.php
+++ htdocs/inc/footer.php
@@ -1,5 +1,6 @@
 	</div>
-	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
+	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + (false && '__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
+	<script type="text/javascript" src="./scripts/colorbox.js"></script>
 	<script type="text/javascript" src="./scripts/script.js"></script>
 </body>
 </html>
