121a69e8cde86df3e71adc4e1c4daa41e606c8ec
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-15 13:07:24 +0330

    fix darkbox for project page

--- htdocs/pages/projects.php
+++ htdocs/pages/projects.php
@@ -6,8 +6,8 @@
 		<section class="mh5 mt4 row cover">
 			<article class="w7 mh9 box right">
 				<h3> پروژه 1</h3>
-				<a href="./images/galleries/original/rug1.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
-					<img src="./images/galleries/thumbs2/rug1.jpg" alt="rug1" width="290" height="210" />
+				<a class="darkbox" href="./images/galleries/projects/1.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/projects/thumbs/1.jpg" alt="rug1" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
@@ -17,8 +17,8 @@
 			</article>
 			<article class="w7 mh9 box mra">
 				<h3> پروژه 2</h3>
-				<a href="./images/galleries/original/rug2.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
-					<img src="./images/galleries/thumbs2/rug2.jpg" alt="rug2" width="290" height="210" />
+				<a class="darkbox" href="./images/galleries/projects/2.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/projects/thumbs/2.jpg" alt="rug2" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
@@ -30,8 +30,8 @@
 		<section class="mh5 mt4 row cover">
 			<article class="w7 mh9 box right">
 				<h3> پروژه 3</h3>
-				<a href="./images/galleries/original/rug3.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
-					<img src="./images/galleries/thumbs2/rug3.jpg" alt="rug3" width="290" height="210" />
+				<a class="darkbox" href="./images/galleries/projects/3.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/projects/thumbs/3.jpg" alt="rug3" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
@@ -41,8 +41,8 @@
 			</article>
 			<article class="w7 mh9 box mra">
 				<h3> پروژه 4</h3>
-				<a href="./images/galleries/original/rug4.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
-					<img src="./images/galleries/thumbs2/rug4.jpg" alt="rug4" width="290" height="210" />
+				<a class="darkbox" href="./images/galleries/projects/1.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/projects/thumbs/1.jpg" alt="rug4" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
@@ -54,8 +54,8 @@
 		<section class="mh5 mt4 row cover">
 			<article class="w7 mh9 box right">
 				<h3> پروژه 5</h3>
-				<a href="./images/galleries/original/rug5.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
-					<img src="./images/galleries/thumbs2/rug5.jpg" alt="rug5" width="290" height="210" />
+				<a class="darkbox" href="./images/galleries/projects/2.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/projects/thumbs/2.jpg" alt="rug5" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
@@ -65,8 +65,8 @@
 			</article>
 			<article class="w7 mh9 box mra">
 				<h3>پروژه 6</h3>
-				<a href="./images/galleries/original/rug6.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
-					<img src="./images/galleries/thumbs2/rug6.jpg" alt="rug6" width="290" height="210" />
+				<a class="darkbox" href="./images/galleries/projects/3.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/projects/thumbs/3.jpg" alt="rug6" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
@@ -78,8 +78,8 @@
 		<section class="mh5 mt5 row cover">
 			<article class="w7 mh9 box right">
 				<h3>پروژه 7</h3>
-				<a href="./images/galleries/original/rug7.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
-					<img src="./images/galleries/thumbs2/rug7.jpg" alt="rug7" width="290" height="210" />
+				<a class="darkbox" href="./images/galleries/projects/1.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/projects/thumbs/1.jpg" alt="rug7" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
@@ -89,8 +89,8 @@
 			</article>
 			<article class="w7 mh9 box mra">
 				<h3>پروژه 8</h3>
-				<a href="./images/galleries/original/rug8.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
-					<img src="./images/galleries/thumbs2/rug8.jpg" alt="rug8" width="290" height="210" />
+				<a class="darkbox" href="./images/galleries/projects/2.jpg" title="Item 1 title ..." data-desc="	Item 1 description " rel="lightbox[group1]">
+					<img src="./images/galleries/projects/thumbs/2.jpg" alt="rug8" width="290" height="210" />
 					<span></span>
 				</a>
 				<p>
