43f20c179a2f0a709763c712dca3e5dde665a72a
Ali aGHasizadeh <me@aligh.pro>
2012-11-19 12:49:13 +0330

    add basic 404 err

--- htdocs/pages/404.php
+++ htdocs/pages/404.php
@@ -1,1 +1,16 @@
-not found
+<div class="content_wrap about">
+	<header class="mh3 mt3 mr1 head">
+		<h1>شرکت مهندسی پارس</h1>
+	</header>
+	
+	<a class="aboutPic darkbox" href="./images/about.jpg">
+		<img  src="./images/notfound.jpg" width="620" height="300" />
+		<span></span>
+	</a>
+	<div class="pl1 pr1 pb4 content">
+		<p>
+خطای 404 : صفحه مورد نظر شما موجود نمی باشد		</p>
+	</div>
+	<?php include 'inc/copy-right.php'; ?>
+	<?php include 'inc/slideshow.php'; ?>
+</div>
