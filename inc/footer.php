e4344548404bc7738cb45b5b87c3d11debe97422
Ali aGHasizadeh <me@aligh.pro>
2012-12-01 17:28:03 +0330

    Add analytic to php

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 	</div>
-	<script type="text/javascript" src="scripts/analytic.js"></script>
 	<?php console_log_show(); ?>
+	<?php include inc('analytic'); ?>
 	<script type="text/javascript" src="./scripts/jquery.js"></script>
 	<script type="text/javascript" src="./scripts/colorbox.js"></script>
 	<script type="text/javascript"> '__proto__' in {} && document.write('<script type="text/javascript" src="./scripts/zepto.js"><\/script>'); </script>
