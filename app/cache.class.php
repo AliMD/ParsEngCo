8f76169b82fd6bd843e6ed6e71b277e5e67a79d5
Ali Mihandoost <i@ali.md>
2012-12-08 10:41:18 +0330

    update to latest version

--- app/cache.class.php
+++ app/cache.class.php
@@ -24,76 +24,62 @@ class MicroCache {
   private $memcache;
 
   function __construct($key=false) {
-    if($key===false) $key = $_SERVER["REQUEST_URI"];
 
-    if(!class_exists('Memcache')) $this->c_type = 'file';
+    class_exists('Memcache') or $this->c_type = 'file';
 
     if($this->c_type != 'file'){
       $this->memcache = new Memcache;
-      if ( !@$this->memcache->connect('localhost', 11211))
-        $this->c_type = 'file';
+      @$this->memcache->connect('localhost', 11211) or $this->c_type = 'file';
     }
 
-    $this->key = md5($key);
+    $this->key = md5( $key===false ? $_SERVER["REQUEST_URI"] : $key);
     $this->file = $this->patch . $this->key . '.cache';
   }
 
   public function check() {
-    if ($this->c_type == 'file') {
-      if ( $this->cache_on and is_readable($this->file) and is_writable($this->file) and time() - filemtime($this->file) < $this->lifetime ) {
-        $this->is_cached = true;
-        return true;
-      }
-      else
-        return false;
-    }
-    else {
-      if ( $this->cache_on and $this->memcache->get($this->key)) {
-        $this->is_cached = true;
-        return true;
-      }
-      else
-        return false;
-    }
+    return $this->is_cached = !$this->cache_on ? false
+      : $this->c_type == 'file' ?
+        ( is_readable($this->file) and is_writable($this->file) and (time()-filemtime($this->file) < $this->lifetime) )
+        : $this->is_cached = ( $this->cache_on and $this->memcache->get($this->key) );
   }
 
   public function out() {
-    if ($this->c_type == 'file') {
-      if ($this->is_cached)
-        return file_get_contents($this->file);
-      else
-        return '';
-    }
-    else {
-      if ($this->is_cached)
-        return $this->memcache->get($this->key);
-      else
-        return '';
-    }
+    return !$this->is_cached ? ''
+      : $this->c_type == 'file' ?
+        file_get_contents($this->file)
+        : $this->memcache->get($this->key);
   }
 
   public function start() {
-      ob_start();
+    ob_start();
   }
 
   public function end() {
     $buffer = ob_get_contents();
     ob_end_clean();
 
-    if ($this->cache_on) {
-      if ($this->c_type == 'file') {
-        $fp = @fopen($this->file, 'w') or die("Cannot create cache file : {$this->file}");
-        if ( @flock($fp, LOCK_EX)) {
-          fwrite($fp, $buffer);
-          fflush($fp);
-          flock($fp, LOCK_UN);
-          fclose($fp);
-        }
-      }
-      else
-        $this->memcache->set($this->key, $buffer, $this->memcache_compressed, $this->lifetime);
-    }
+    $this->cache_on and $this->write($buffer);
+
     die ($buffer);
+  }
+
+  public function write($buffer = ''){
+    if($this->c_type == 'file') {
+      $fp = @fopen($this->file, 'w') or die("Cannot create cache file : {$this->file}");
+      if ( @flock($fp, LOCK_EX)) {
+        fwrite($fp, $buffer);
+        fflush($fp);
+        flock($fp, LOCK_UN);
+        fclose($fp);
+      }
+    } else $this->memcache->set($this->key, $buffer, $this->memcache_compressed, $this->lifetime);
+  }
+
+  public function clear($key=false){
+    $key = $key === false ? $this->key : md5($key);
+    $file = $this->patch . $this->key . '.cache';
+    if ($this->c_type == 'file') @unlink($file);
+    else $this->memcache->delete($key);
   }
 
 }
