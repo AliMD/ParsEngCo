e1ba3c03cd3a0594b95d461c2ed73233af3a75fb
Ali aGHasizadeh <me@aligh.pro>
2013-08-13 11:07:55 +0430

    add parseng sms sending

--- inc/footer.php
+++ inc/footer.php
@@ -1,2 +1,11 @@
+<div class="suggest">
+  <p><i class="icon-share"></i>ثبت نام در خبرنامه</p>
+  <form action="share.php" id="share" method="post" target='share-frm'>
+    <input type="text" name="name" id="share-name" placeholder="نام خود را وارد کنید"/>
+    <input type="text" name="email" id="share-email" placeholder="شماره تلفن خود را وارد نمایید"/>
+    <input type="submit" name="submit" value="ارسال" />
+  </form>
+  <iframe id='share-frm' name='share-frm'></iframe>
+</div>
 </body>
 </html>
