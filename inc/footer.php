5e5808f307de896eedcafdc5d4e94978d62f89f2
Ali aGHasizadeh <me@aligh.pro>
2013-08-17 09:22:57 +0430

    fix footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
-<div class="suggest">
-  <p><i class="icon-share"></i>ثبت نام در خبرنامه</p>
-  <form action="share.php" id="share" method="post" target='share-frm'>
+<div class="suggest active">
+  <p><i class="icon-share"></i>در دسترس باشید</p>
+  <form action="../share.php" id="share" method="post" target='share-frm'>
     <input type="text" name="name" id="share-name" placeholder="نام خود را وارد کنید"/>
     <input type="text" name="email" id="share-email" placeholder="شماره تلفن خود را وارد نمایید"/>
     <input type="submit" name="submit" value="ارسال" />
