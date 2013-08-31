7a555a8b0da1235f6119df96e55918ee8a4275ce
Ali aGHasizadeh <me@aligh.pro>
2013-08-31 12:52:36 +0430

    fix share phone

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,8 @@
 <div class="suggest active">
   <p><i class="icon-share"></i>در دسترس باشید</p>
-  <form action="../share.php" id="share" method="post" target='share-frm'>
+  <form action="share.php" id="share" method="post" target='share-frm'>
     <input type="text" name="name" id="share-name" placeholder="نام خود را وارد کنید"/>
-    <input type="text" name="email" id="share-email" placeholder="شماره تلفن خود را وارد نمایید"/>
+    <input type="text" name="smsphone" id="share-phone" placeholder="شماره تلفن خود را وارد نمایید"/>
     <input type="submit" name="submit" value="ارسال" />
   </form>
   <iframe id='share-frm' name='share-frm'></iframe>
